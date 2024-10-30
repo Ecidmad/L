b93f290b0d3b3871cacc9cdf155b833e9d11ee56
Ecidmad <167511670+Ecidmad@users.noreply.github.com>
2024-10-31 01:35:03 +0300

    Add files via upload

---
+++ L22/L22.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x;
+    int s = 0;
+    int i = 1;
+    while (true)
+    {
+        std::cout << "Введите натуральное число: ";
+        std::cin >> x;
+        if (x > 0)
+        {
+            break;
+        }
+    }
+    while(i<=x)
+    {
+        if (i == 1)
+        {
+            std::cout << i;
+        }
+        else 
+        {
+            std::cout << " + " << i;
+        }
+        s = s + i;
+        i++;
+    }
+    i = 0;
+    std::cout << " = " << s << "\n";
+    int array[] = { 1, 2, 3, 5, 6, 7, 8, 9, 10, 4 };
+    for (int r : array) 
+    {
+        std::cout << r << " ";
+    }
+    std::cout << "\n";
+    for (int r : array) 
+    {
+        if (i % 2 == 0) 
+        {
+        std::cout << r << " ";
+        }
+        i++;
+    }
+    std::cout << "\n";
+    i = 0;
+    s = 0;
+    for (int r : array) 
+    {
+        if (i % 2 == 1) 
+        {
+            s = s + r;
+        }
+        i++;
+    }
+    std::cout << s;
+}
