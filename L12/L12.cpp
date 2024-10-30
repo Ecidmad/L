29cc877d321fb065ce626dc567de8a124ced40fb
ecidm <ecidm@DESKTOP-I8F7FUA>
2024-10-30 20:11:43 +0300

    Добавьте файлы проекта.

---
+++ L12/L12.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x = 150;
+    double y = 15.933f;
+    short z = 250;
+    std::cout << " x = " << x << "\n";
+    std::cout << " y = " << y << "\n";
+    std::cout << " z = " << z << "\n";
+    short day = 6;
+    std::string month = "Мая";
+    short year = 2005;
+    std::cout << "Моя дата рождения: " << day << " " << month << " " << year << " года\n";
+    const double Num = 2.3f;
+    const std::string Str = "WINDOWS";
+    std::cout << Num << " " << Str << "\n";
+}
