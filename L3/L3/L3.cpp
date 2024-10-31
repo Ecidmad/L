3095af8a4d544c319a1c6d5ec0cbc011a3198517
Ecidmad <167511670+Ecidmad@users.noreply.github.com>
2024-10-31 12:13:45 +0300

    Add files via upload

---
+++ L3/L3/L3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main()
+{
+    int x = 60;
+    int* pointer;
+    pointer = &x;
+    std::cout << "pointer = " << pointer << "\n" << "*pointer = " << *pointer << "\n";
+    *pointer = 1;
+    std::cout << x << "\n";
+    int j[2][2] = { {4,0},{1,2} };
+    int f = sizeof(j) / sizeof(j[0]);
+    int s = sizeof(j[0]) / sizeof(j[0][0]);
+    int* e = j[0] + f * s - 1;
+    for (int* q = j[0]; q <= e; q++) 
+    {
+        std::cout << *q << " ";
+    }
+    std::cout << "\n";
+    int r = 5;
+    int* const u = &r;
+    std::cout << r << " " << u << " " << *u << "\n";
+    *u = 100;
+    std::cout << r << " " << u << " " << *u << "\n";
+    r = 60;
+    std::cout << r << " " << u << " " << *u << "\n";
+}
