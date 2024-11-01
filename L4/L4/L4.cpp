63d477e658d536cab93b70762996c6dd87aa9ef3
Ecidmad <167511670+Ecidmad@users.noreply.github.com>
2024-11-02 00:08:34 +0300

    Add files via upload

---
+++ L4/L4/L4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using std::string;
+class Weapon
+{
+public:
+    string name;
+    float damage;
+    int weight;
+    Weapon(string name, float damage, int weight) : name(name), damage(damage), weight(weight) {}
+    Weapon() :Weapon("Weapon", 1, 1){}
+};
+int main()
+{
+    Weapon Bow("Bow", 10, 2);
+    std::cout << Bow.name << Bow.damage << Bow.weight << "\n";
+    Weapon Sword;
+    std::cout << Sword.name << Sword.damage << Sword.weight << "\n";
+}
