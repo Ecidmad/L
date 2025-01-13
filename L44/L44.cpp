79fc7b6e625558d0223dcfca00dc8adbce53232f
Ecidmad <Ecidmad@yandex.ru>
2025-01-13 22:41:49 +0300

    Point 4

--- L44/L44.cpp
+++ L44/L44.cpp
@@ -4,28 +4,16 @@ using namespace std;
 #include "Characteristic.h";
 #include "MyMath.h";
 int MyMath::K = 0;
+
+struct Player {
+    int id;
+    string login;
+    string password;
+};
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    /* 
-    Weapon Bow("Bow", 10, 2);
-    Weapon Sword;
-    Weapon Spear("Spear", 25, 6);
-    Weapon* Shotgun = new Weapon("Shotgun", 220, 6);
-    Bow.maxweight(12);
-    Spear.sumweight(Bow);
-    Spear.sumweight(30);
-    Bow.setDamage(30);
-    cout << Bow.getDamage() << "\n";
-    Characteristic hand(5);
-    cout << hand.GetDamage(Shotgun) << "\n";
-    delete Shotgun;
-    cout << MyMath::Add(6, 4) << "\n";
-    cout << MyMath::Div(8, 4) << "\n";
-    cout << MyMath::Mult(6, 4) << "\n";
-    cout << MyMath::Sub(6, 4) << "\n";
-    cout << MyMath::K << "\n"; 
-    */
     Weapon::printGroup(Direction::ONEHANDED);
     Weapon::printGroup(Direction::TWOHANDED);
     Weapon::printGroup(Direction::BOW);
