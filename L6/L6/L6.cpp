b42874681e44c5b863833bd03d33099e6f37102f
Ecidmad <Ecidmad@yandex.ru>
2025-01-14 20:56:18 +0300

    Point 1, 2

---
+++ L6/L6/L6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int number;
+    cout << "Введите число от одного до трех: \n";
+    cin >> number;
+    try
+    {
+        switch (number) {
+        case 1:
+            cout << "Один" << "\n";
+            break;
+        case 2:
+            cout << "Два" << "\n";
+            break;
+        case 3:
+            cout << "Три" << "\n";
+            break;
+        default:
+            throw "Некорректное значение";
+        }
+    }
+    catch (const char* msg)
+    {
+        cout << msg << "\n";
+    }
+}
