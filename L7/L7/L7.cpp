8e87c7e2e1c05a7736cecafaa7f945c05195c673
Ecidmad <Ecidmad@yandex.ru>
2025-01-15 08:10:58 +0300

    Point 2

--- L7/L7/L7.cpp
+++ L7/L7/L7.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <regex>
 using namespace std;
 int main()
 {
@@ -7,4 +8,13 @@ int main()
     string userName;
     cout << "Введите Ваше имя: \n";
     getline(cin, userName);
+    regex nameRegex("^[A-Z][a-zA-Z]{1,31}$");
+    if (regex_match(userName, nameRegex))
+    {
+        cout << "Имя введено корректно.\n";
+    }
+    else
+    {
+        cout << "Имя некорректно! Имя должно быть от 2 до 32 символов и начинаться с заглавной буквы.\n";
+    }
 }
