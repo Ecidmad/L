93479d61d7ca75dd02625fba539be91699cb155a
Ecidmad <Ecidmad@yandex.ru>
2025-01-15 07:23:03 +0300

    Point 1

---
+++ L7/L7/L7.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string userInput;
+    string got;
+    cout << "Введите строку: \n";
+    getline(cin, userInput);
+    cout << userInput;
+}
