0737266566b77a4d3202082a4c43d98124bd5b82
Ecidmad <Ecidmad@yandex.ru>
2025-01-15 09:09:18 +0300

    Lab 7.3

--- L7/L7/L7.cpp
+++ L7/L7/L7.cpp
@@ -1,27 +1,72 @@
 ﻿#include <iostream>
 #include <string>
-#include <regex>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
+const double PI = 3.141592653589793;
 int main()
 {
     setlocale(LC_ALL, "");
-    string userName;
-    cout << "Введите Ваше имя: \n";
-    getline(cin, userName);
-    regex nameRegex("^[A-Z][a-zA-Z]{1,31}$");
-    if (regex_match(userName, nameRegex))
+    //double radius;
+    //cout << "Введите радиус окружности: ";
+    //cin >> radius;
+    //double circumference = 2 * PI * radius;
+    //cout << "Длина окружности: " << round(circumference * 1000) / 1000 << endl;
+    string fileName = "output.txt";
+    string userInput;
+    while (true)
     {
-        cout << "Имя введено корректно.\n";
-    }
-    else
-    {
-        cout << "Имя некорректно! Имя должно быть от 2 до 32 символов и начинаться с заглавной буквы.\n";
-    }
-    string text = "Пример текста с e-mail адресами: john.doe@examplecom, alice.smith@company.org и support@mydomain.net.";
-    regex emailRegex("^[a-zA-Z0-9_][a-zA-Z0-9_\.-]*[a-zA-Z0-9_]@([a-z0-9]+[a-z0-9_-]*[a-z0-9]\.)+[a-z0-9]{2,}$", regex_constants::icase);
-    smatch matches;
-    while (regex_search(text, matches, emailRegex))
-    {
-        cout << matches[1] << "\n";
+        cout << "Введите строку (или команды read, erase, exit): ";
+        getline(cin, userInput);
+        if (userInput == "read")
+        {
+            ifstream file(fileName);
+            if (file.is_open())
+            {
+                cout << "Содержимое файла:\n";
+                string line;
+                while (getline(file, line)) {
+                    cout << line << endl;
+                }
+                file.close();
+            }
+            else
+            {
+                cout << "Файл пуст или отсутствует." << endl;
+            }
+        }
+        else if (userInput == "erase")
+        {
+            ofstream file(fileName, ios::trunc);
+            if (file.is_open())
+            {
+                cout << "Файл очищен." << endl;
+                file.close();
+            }
+            else
+            {
+                cerr << "Ошибка при очистке файла." << endl;
+            }
+        }
+        else if (userInput == "exit")
+        {
+            cout << "Завершение программы." << endl;
+            break;
+        }
+        else
+        {
+            ofstream file(fileName, ios::app);
+            if (file.is_open())
+            {
+                file << userInput << endl;
+                file.close();
+                cout << "Строка добавлена в файл." << endl;
+            }
+            else
+            {
+                cerr << "Ошибка при записи в файл." << endl;
+            }
+        }
     }
 }
