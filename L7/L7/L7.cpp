d9e59babb1e0d3597e3fa720693d8e5703fa66ee
Ecidmad <Ecidmad@yandex.ru>
2025-01-15 07:42:35 +0300

    Point 1

--- L7/L7/L7.cpp
+++ L7/L7/L7.cpp
@@ -3,21 +3,8 @@
 using namespace std;
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    string userInput;
-    string got;
-    cout << "Введите строку: \n";
-    getline(cin, userInput);
-    cout << userInput << "\n";
-    if (userInput.length() >= 4)
-    {
-        cout << "Подстрока с 2-го по 4-й символ: " << userInput.substr(1, 3) << "\n";
-    }
-    else
-    {
-        cout << "В строке слишком мало символов, нужно не менее 4 символов.\n";
-    }
-    int index = userInput.find("a");
-    if (index == -1) cout << "Строка не содержит символ 'a'.\n";
-    else cout << "Символ 'a' стоит на " << index+1 << " месте\n";
+    setlocale(LC_ALL, "");
+    string userName;
+    cout << "Введите Ваше имя: \n";
+    getline(cin, userName);
 }
